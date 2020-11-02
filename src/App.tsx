7a5fffd318246be97296bcd39c6860f9cd3982c2
Vikum Wijekoon <vikumwijekoon97@gmail.com>
2020-11-02 07:34:23 +0530

    Opportunity Card design

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import { useQuery, gql } from "@apollo/client";
+import OppCard from "./features/OppCard/OppCard";
+import { Container } from "react-bootstrap";
+const client = new ApolloClient({
+  uri: "https://48p1r2roz4.sse.codesandbox.io",
+  cache: new InMemoryCache(),
+});
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const EXCHANGE_RATES = gql`
+  query GetExchangeRates {
+    rates(currency: "USD") {
+      currency
+      rate
+    }
+  }
+`;
+
+function ExchangeRates() {
+  const { loading, error, data } = useQuery(EXCHANGE_RATES);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return data.rates.map(({ currency, rate }: { currency: any; rate: any }) => (
+    <div key={currency}>
+      <p>
+        {currency}: {rate}
+      </p>
     </div>
+  ));
+}
+function App() {
+  const [favorited, setFavorited] = useState(false);
+  return (
+    <ApolloProvider client={client}>
+      <div className="App">
+        <Container fluid="md" className="p-3">
+          <OppCard favorited={favorited} setFavorited={setFavorited}></OppCard>
+        </Container>
+      </div>
+    </ApolloProvider>
   );
 }
 
---
+++ src/features/OppCard/OppCard.tsx
@@ -0,0 +1,83 @@
+import React, { useEffect, useState } from "react";
+import { Button, Container, Media } from "react-bootstrap";
+import { useMediaQuery } from "react-responsive";
+enum variantColorEnum {
+  outlineDanger = "outline-danger",
+  danger = "danger",
+}
+export default function OppCard({
+  favorited,
+  setFavorited,
+}: {
+  favorited: boolean;
+  setFavorited: Function;
+}) {
+  const [variantColor, setVariantColor] = useState(
+    variantColorEnum.outlineDanger
+  );
+  const isMobile: boolean = useMediaQuery({ maxWidth: 576 });
+  useEffect(() => {
+    if (favorited === true) {
+      setVariantColor(variantColorEnum.danger);
+    } else {
+      setVariantColor(variantColorEnum.outlineDanger);
+    }
+  }, [favorited]);
+  const allP: string = isMobile ? "p-2" : "p-3";
+  const topP: string = isMobile ? "pt-3" : "pt-2";
+  const sideP: string = isMobile ? "px-2" : "";
+  const botM: string = isMobile ? "mb-1" : "mb-2";
+  const textW: string = isMobile ? "" : "w-85";
+  const product = "gv";
+  //   const;
+  return (
+    <Container className={`border ${allP} opp-card`}>
+      <Button
+        className="opp-card__save-btn"
+        variant={variantColor}
+        onClick={() => setFavorited(!favorited)}>
+        {favorited && <i className="fas fa-heart"></i>}
+        {!favorited && <i className="far fa-heart"></i>}
+
+        {/* <i className="material-icons md-24 opp-card__save-btn--borderless">
+          favorite_border
+        </i>
+        <i
+          className="material-icons md-24 opp-card__save-btn--filled"
+          style={{ color: btnColor }}>
+          favorite
+        </i> */}
+      </Button>
+      <Media className="flex-wrap">
+        <img
+          className=" rounded mr-4"
+          width={isMobile ? `100%` : `30%`}
+          height="160px"
+          style={{ objectFit: "cover" }}
+          src="https://cdn-expa.aiesec.org/gis-img/gv_default.png"
+          alt="Generic placeholder"
+        />
+        <Media.Body className={`${topP} ${sideP} `}>
+          {/* <p className="mb-1">
+            <small className={`text-${product}`}>Global Volunteer</small>
+          </p> */}
+          <div className="d-flex justify-content-between">
+            <h5>Media Heading</h5>
+          </div>
+          <p className={`${botM} text-muted f14`}>
+            Country Somewhere, 6 to 8 weeks
+          </p>
+          <p className={`${botM} ${textW}`}>
+            {" "}
+            Some descrption here fdssfds sdfgdsfsf sdf sdf sf sf sdsd f fsd fdsf
+            sfd s s fs fds fs fs fs fsd fs fd sf sdf sdfs d fs f sf ds fs df{" "}
+          </p>
+          <div className="d-flex justify-content-between">
+            <p className={`${botM} text-muted f14`}>Some other Details</p>
+            <p className="${botM} text-muted f14 "> sdsfsdfsd</p>
+          </div>
+        </Media.Body>
+      </Media>
+    </Container>
+  );
+}
