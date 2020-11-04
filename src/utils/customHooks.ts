8efde4f558fb9814c5aed34260a12f5a73510004
Vikum Wijekoon <vikumwijekoon97@gmail.com>
2020-11-05 05:09:37 +0530

    added isSmallAF media query

--- src/utils/customHooks.ts
+++ src/utils/customHooks.ts
@@ -2,12 +2,15 @@ import { useMediaQuery } from 'react-responsive'
 
 interface IUseResponsive {
     isXS: boolean,
-    isLG: boolean
+    isLG: boolean,
+    isSmallAF: boolean
 }
 export const useResponsive = (): IUseResponsive => {
     const isXS: boolean = useMediaQuery({ maxWidth: 576 });
     const isLG: boolean = useMediaQuery({ maxWidth: 992 })
-    return { isXS, isLG }
+    const isSmallAF: boolean = useMediaQuery({ maxWidth: 361 })
+
+    return { isXS, isLG, isSmallAF }
 }
 // export const useLg = (): boolean => {
 //     return isLg
